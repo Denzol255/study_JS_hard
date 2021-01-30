5d27331cecab0ddbb8034466308cbfe39ebc6798
Denzol255 <den-zoloto@mail.ru>
2021-01-30 17:34:48 +0300

    Fix: some fixes

--- js/script.js
+++ js/script.js
@@ -7,12 +7,10 @@ let total = numberArray.reduce(function (a, b) {
   return a * b;
 });
 
+const totalArray = total.toString().split("");
+
 console.log(total);
 
 total = total ** 3;
 
-console.log(total);
-
-const totalArray = total.toString().split("");
-
-alert(`Первое число ${totalArray[0]}, второе число ${totalArray[1]}!`);
+console.log(`Первое число ${totalArray[0]}, второе число ${totalArray[1]}!`);
