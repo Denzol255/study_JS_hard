3c5487d5e1fc10f5ac8497358bda818deb670561
Denzol255 <den-zoloto@mail.ru>
2021-02-01 19:30:22 +0300

    Create: solution with IF

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 "use strict";
 
-const num = 266219;
-const numberArray = num.toString().split("");
+// const num = 266219;
+// const numberArray = num.toString().split("");
 
-let total = numberArray.reduce(function (a, b) {
-  return a * b;
-});
+// let total = numberArray.reduce(function (a, b) {
+//   return a * b;
+// });
 
-const totalArray = total.toString().split("");
+// const totalArray = total.toString().split("");
 
-console.log(total);
+// console.log(total);
 
-total = total ** 3;
+// total = total ** 3;
 
-console.log(`Первое число ${totalArray[0]}, второе число ${totalArray[1]}!`);
+// console.log(`Первое число ${totalArray[0]}, второе число ${totalArray[1]}!`);
+
+let lang = prompt("Выберите язык (введите 'ru' или 'eng')");
+
+if (lang === "ru") {
+  console.log(
+    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье!"
+  );
+} else if (lang === "eng") {
+  console.log(
+    "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday!"
+  );
+} else {
+  console.log("Вы неправильно выбрали язык");
+}
